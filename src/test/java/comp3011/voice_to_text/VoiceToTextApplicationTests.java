7ae3299e59038f35abf175447dd2426b4aa4f687
Phoenix-Dong02 <a1912655@adelaide.edu.au>
2026-08-18 18:47:22 +0930

    init spring boot project

---
+++ src/test/java/comp3011/voice_to_text/VoiceToTextApplicationTests.java
@@ -0,0 +1,13 @@
+package comp3011.voice_to_text;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceToTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
